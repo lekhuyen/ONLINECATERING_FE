9618e7c1e65a3e4e6c7ee85379fe51db4af3499a
PhamDuyHau <phamduyhau2000@gmail.com>
2024-07-19 08:35:37 +0700

    7/19 demo INFORMATION FE
    7/19 demo INFORMATION FE

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,7 +16,7 @@ const publicRoutes = [
     {path: "/", component: Home},
     {path: "/about", component: About},
     {path: "/login", component: Login, layout: null},
-    {path: "/contact", component: Contact},
+    {path: "/contact-us", component: Contact},
     {path: "/news", component: News},
     {path: "/account", component: Account},
 ]
