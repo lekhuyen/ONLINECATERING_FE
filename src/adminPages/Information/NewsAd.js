489a0af09a7a031a2e78ce7d1bbe357421ac1c85
PhamDuyHau <phamduyhau2000@gmail.com>
2024-07-20 14:52:07 +0700

    Add more

--- src/adminPages/Information/NewsAd.js
+++ src/adminPages/Information/NewsAd.js
@@ -1,8 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import { deleteNewsItem, fetchNewsData } from '../../redux/Information/newsSlice';
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import {
+  deleteNewsItem,
+  fetchNewsData,
+} from "../../redux/Information/newsSlice";
+import '@fortawesome/fontawesome-free/css/all.css'; // Import Font Awesome CSS
 
 function NewsAd() {
   const dispatch = useDispatch();
@@ -26,7 +30,7 @@ function NewsAd() {
       await axios.delete(`http://localhost:5034/api/News/${id}`);
       dispatch(deleteNewsItem(id));
     } catch (error) {
-      console.error('Error deleting item:', error);
+      console.error("Error deleting item:", error);
     }
   };
 
@@ -61,19 +65,24 @@ function NewsAd() {
 
   // Function to limit content to at least 9 words with ellipses
   const limitContent = (content) => {
-    const words = content.split(' ');
+    const words = content.split(" ");
     if (words.length > 10) {
-      return words.slice(0, 10).join(' ') + '...';
+      return words.slice(0, 10).join(" ") + "...";
     }
     return content;
   };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h2>News Data</h2>
-      <button className="btn btn-success mb-3" onClick={() => navigate('/Create-News')}>Create News</button>
+      <button
+        className="btn btn-success mb-3"
+        onClick={() => navigate("/Create-News")}
+      >
+        Create News
+      </button>
 
-      <table className="table table-dark">
+      <table className="table table-hover">
         <thead>
           <tr>
             <th>Id</th>
@@ -96,45 +105,105 @@ function NewsAd() {
                   <img
                     src={`http://localhost:5034${news.imagePaths[0]}`}
                     alt={`News ${news.id}`}
-                    style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+                    style={{
+                      width: "100px",
+                      height: "100px",
+                      objectFit: "cover",
+                    }}
                   />
                 )}
-                <i class="fa fa-info-circle" aria-hidden="true"></i>
               </td>
               <td>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleEdit(news.id)}
-                >
-                  Edit
+                <button className="btn btn-outline-primary">
+                  <i class="fa fa-info-circle" aria-hidden="true"></i>
                 </button>
                 <button
-                  className="btn btn-danger ml-2"
+                  className="btn btn-outline-warning"
+                  onClick={() => handleEdit(news.id)}
+                >
+                  <i class="fa fa-pencil-square" aria-hidden="true"></i>
+                </button>
+                <button
+                  className="btn btn-outline-danger"
                   onClick={() => handleDelete(news.id)}
                 >
-                  Delete
+                  <i class="fa fa-trash" aria-hidden="true"></i>
                 </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      
+      <button
+        type="button"
+        class="btn btn-info btn-lg"
+        data-toggle="modal"
+        data-target="#myModal"
+      >
+        Open Modal
+      </button>
+
+      {/* <!-- Modal --> */}
+      <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+          {/* <!-- Modal content--> */}
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">
+                &times;
+              </button>
+              <h4 class="modal-title">Modal Header</h4>
+            </div>
+            <div class="modal-body">
+              <p>Some text in the modal.</p>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-default"
+                data-dismiss="modal"
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* Pagination */}
       <nav>
         <ul className="pagination">
-          <li className={`page-item ${currentPage === 1 ? 'disabled' : ''}`}>
-            <button className="page-link" onClick={() => paginate(currentPage - 1)}>Previous</button>
+          <li className={`page-item ${currentPage === 1 ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage - 1)}
+            >
+              Previous
+            </button>
           </li>
           {Array.from({ length: pageNumbers }, (_, index) => (
-            <li key={index} className={`page-item ${currentPage === index + 1 ? 'active' : ''}`}>
+            <li
+              key={index}
+              className={`page-item ${
+                currentPage === index + 1 ? "active" : ""
+              }`}
+            >
               <button className="page-link" onClick={() => paginate(index + 1)}>
                 {index + 1}
               </button>
             </li>
           ))}
-          <li className={`page-item ${currentPage === pageNumbers ? 'disabled' : ''}`}>
-            <button className="page-link" onClick={() => paginate(currentPage + 1)}>Next</button>
+          <li
+            className={`page-item ${
+              currentPage === pageNumbers ? "disabled" : ""
+            }`}
+          >
+            <button
+              className="page-link"
+              onClick={() => paginate(currentPage + 1)}
+            >
+              Next
+            </button>
           </li>
         </ul>
       </nav>
